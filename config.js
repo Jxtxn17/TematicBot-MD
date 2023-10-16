e208027fe07e0954e2cf6aab83c22c32f5a01358
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2023-10-15 21:24:23 -0500

    Actualizar config.js

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ global.mods = []
 global.prems = []
 
 global.packname = '× Stickers V2\ngithub.com/TurboBot-MD\n\n× Script:\ngithub.com/Jxtxn17/TurboBot-MD'
-global.author = ' \n U\n R\n  B\n O\n '
+global.author = ' T\n U\n R\n  B\n O\n '
 global.wm = '𓆩𝐓𝐮𝐫𝐛𝐨𝐁𝐨𝐭-𝐌𝐃𓆪'
 global.wm2 = '✦𝗧𝘂𝗿𝗯𝗼-𝗯𝗼𝘁-𝗠𝗗✦'
 global.jxtxn = 'Jxtxn'
