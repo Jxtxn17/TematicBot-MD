eb6dabb431a36ff987035695bab704e94721eb60
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2023-10-15 21:38:58 -0500

    Actualizar config.js

--- config.js
+++ config.js
@@ -1,23 +1,23 @@
-import { watchFile, unwatchFile } from 'fs'
-import chalk from 'chalk'
-import fs from 'fs'
-import cheerio from 'cheerio'
-import fetch from 'node-fetch'
-import axios from 'axios'
-import path, { join } from 'path'
-import { fileURLToPath, pathToFileURL } from 'url'
-import moment from 'moment-timezone' 
-import { platform } from 'process'
-global.__filename = function filename(pathURL = import.meta.url, rmPrefix = platform !== 'win32') { return rmPrefix ? /file:\/\/\//.test(pathURL) ? fileURLToPath(pathURL) : pathURL : pathToFileURL(pathURL).toString() }; global.__dirname = function dirname(pathURL) { return path.dirname(global.__filename(pathURL, true)) }; global.__require = function require(dir = import.meta.url) { return createRequire(dir) }
-const __dirname = global.__dirname(import.meta.url)
 
-global.owner = [['51929972576', 'Creador', true]
-
-global.animxscans = ['400359205']
-global.suittag = ['994400359205']
-global.mods = []
-global.prems = []
-
+import { watchFile, unwatchFile } from 'fs' 
+ import chalk from 'chalk' 
+ import fs from 'fs' 
+ import cheerio from 'cheerio' 
+ import fetch from 'node-fetch' 
+ import axios from 'axios' 
+ import path, { join } from 'path' 
+ import { fileURLToPath, pathToFileURL } from 'url' 
+ import moment from 'moment-timezone'  
+ import { platform } from 'process' 
+ global.__filename = function filename(pathURL = import.meta.url, rmPrefix = platform !== 'win32') { return rmPrefix ? /file:\/\/\//.test(pathURL) ? fileURLToPath(pathURL) : pathURL : pathToFileURL(pathURL).toString() }; global.__dirname = function dirname(pathURL) { return path.dirname(global.__filename(pathURL, true)) }; global.__require = function require(dir = import.meta.url) { return createRequire(dir) } 
+ const __dirname = global.__dirname(import.meta.url) 
+  
+ global.owner = [['527294888993', 'Creador', true], ['595975740803', 'Colaborador', true], ['593968585383'], ['51992004117'], ['5492266613038'], ['573106040746'], ['50576390682'], ['5217294888993'], ['50558124470']] 
+  
+ global.animxscans = ['400359205'] 
+ global.suittag = ['994400359205'] 
+ global.mods = [] 
+ global.prems = []
 global.packname = '× Stickers V2\ngithub.com/TurboBot-MD\n\n× Script:\ngithub.com/Jxtxn17/TurboBot-MD'
 global.author = ' T\n U\n R\n  B\n O\n '
 global.wm = '𓆩𝐓𝐮𝐫𝐛𝐨𝐁𝐨𝐭-𝐌𝐃𓆪'
