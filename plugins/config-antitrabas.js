df44e3c5a29ce25c403894a9b23ac444a18a22c2
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2023-10-18 20:11:31 -0500

    Actualizar config-antitrabas.js

--- plugins/config-antitrabas.js
+++ plugins/config-antitrabas.js
@@ -11,7 +11,8 @@ let bang = m.key.id
 let name = await conn.getName(m.sender)
 if (chat.antiTraba && m.text.length > 5000) { //Cantidad máxima de caracteres aceptados en un mensaje//
 if (isAdmin) return conn.sendMessage(m.chat, { text: `El administrador @${m.sender.split("@")[0]} acaba de enviar un texto que contiene muchos caracteres -.-!`, mentions: [m.sender] }, { quoted: fakemek })
-await conn.sendMessage(m.chat, `*⚠️ Se detecto un mensaje que contiene muchos caracteres ⚠️*\n`, `${isBotAdmin ? '' : 'No soy administrador, no puedo hacer nada :/'}`, m);
+await conn.sendMessage(m.chat, `*[ 🍓 ] 𝙀𝙎𝙏𝙀 𝙈𝙀𝙉𝙎𝘼𝙅𝙀 𝙏𝙄𝙀𝙉𝙀 𝙈𝙐𝘾𝙃𝙊𝙎 𝘾𝘼𝙍𝘼𝘾𝙏𝙀𝙍𝙀𝙎️*\n`, `${isBotAdmin ?'
+[ 🍥 ]𝙉𝙊 𝙎𝙊𝙔 𝘼𝘿𝙈𝙄𝙉 𝙉𝙊 𝙋𝙐𝙀𝘿𝙊 𝙃𝘼𝘾𝙀𝙍𝙈𝙀 𝘾𝘼𝙍𝙂𝙊/'}`, m);
 if (isBotAdmin && bot.restrict) {
 conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
 setTimeout(() => { 
@@ -20,7 +21,7 @@ conn.sendMessage(m.chat, { text: `Marcar el chat como leido ✓\n${"\n".repeat(4
 setTimeout(() => { 
 conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
 }, 1000)
-} else if (!bot.restrict) return m.reply(`*⚠️ MI PROPIETARIO DEBE TENER EL MODO RESTRINGIDO ACTIVO*`)
+} else if (!bot.restrict) return m.reply(`*⚠️ 𝙈𝙄 𝘿𝙀𝙎𝘼𝙍𝙍𝙊𝙇𝙇𝘼𝘿𝙊𝙍 𝘿𝙀𝘽𝙀 𝙏𝙀𝙉𝙀𝙍 𝙀𝙇 𝙈𝙊𝘿𝙊 𝙍𝙀𝙎𝙏𝙍𝙄𝙉𝙂𝙄𝘿𝙊 𝘼𝘾𝙏𝙄𝙑𝙊*`)
 }
 return !0
 
