8526adeb2c5db13112362b054be5f6e4c05fe1b1
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2023-10-19 21:06:18 -0500

    Actualizar grupo-setdesc.js

--- plugins/grupo-setdesc.js
+++ plugins/grupo-setdesc.js
@@ -2,8 +2,8 @@ var handler = async (m, { conn, text, isROwner, isOwner }) => {
 
 if (text) {
 await conn.groupUpdateDescription(m.chat, text)
-m.reply(`*✅ SE A CAMBIADO LA DESCRIPCIÓN*`)
-} else throw '*⚠️ QUE DESCRIPCIÓN PONGO?*'
+m.reply(`*✅ 𝙎𝙀 𝘼 𝘾𝘼𝙈𝘽𝙄𝘼𝘿𝙊 𝙇𝘼 𝘿𝙀𝙎𝘾𝙍𝙄𝙋𝘾𝙄𝙊́𝙉*`)
+} else throw '*[❗𝐈𝐍𝐅𝐎❗]𝙌𝙐𝙀 𝘿𝙀𝙎𝘾𝙍𝙄𝙋𝘾𝙄𝙊́𝙉 𝙋𝙊𝙉𝙂𝙊?*'
 
 }
 handler.help = ['setdesc']
