e3b6bcb8538a508acfe85bbea896b733076ae0ae
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2023-10-19 21:05:23 -0500

    Actualizar grupo-setpp.js

--- plugins/grupo-setpp.js
+++ plugins/grupo-setpp.js
@@ -4,9 +4,9 @@ let q = m.quoted ? m.quoted : m
 let mime = (q.msg || q).mimetype || q.mediaType || ''
 if (/image/.test(mime)) {
 let img = await q.download()
-if (!img) throw '*⚠️️ RESPONDE A UNA IMAGEN*'
+if (!img) throw '* [❗𝐈𝐍𝐅𝐎❗] 𝙍𝙀𝙎𝙋𝙊𝙉𝘿𝙀 𝘼 𝙐𝙉𝘼 𝙄𝙈𝘼𝙂𝙀𝙉*'
 await conn.updateProfilePicture(m.chat, img).then(_ => m.reply('❕ *_Imagen actualizada con éxito_*'))
-} else throw '*⚠️️ RESPONDE A UNA IMAGEN*'
+} else throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙍𝙀𝙎𝙋𝙊𝙉𝘿𝙀 𝘼 𝙐𝙉𝘼 𝙄𝙈𝘼𝙂𝙀𝙉*'
 
 }
 handler.command = /^setpp(group|grup|gc)?$/i
