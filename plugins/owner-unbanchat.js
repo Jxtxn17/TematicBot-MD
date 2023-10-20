7063148c4f041defd51d9cb6df7933f058cf8d05
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2023-10-19 21:19:50 -0500

    Actualizar owner-unbanchat.js

--- plugins/owner-unbanchat.js
+++ plugins/owner-unbanchat.js
@@ -1,6 +1,6 @@
 let handler = async (m) => {
 global.db.data.chats[m.chat].isBanned = false
-m.reply('*[💎] ESTE CHAT FUE  DESBANEADO [💎]*\n\n*🍓 ESTE CHAT YA TIENE PERMITIDO USARME🍓*')
+m.reply('*[❗𝐈𝐍𝐅𝐎❗] 𝙀𝙎𝙏𝙀 𝘾𝙃𝘼𝙏 𝙁𝙐𝙀 𝘿𝙀𝙎𝘽𝘼𝙉𝙀𝘼𝘿𝙊 [❗𝐈𝐍𝐅𝐎❗]*\n\n* 𝙀𝙎𝙏𝙀 𝘾𝙃𝘼𝙏 𝙏𝙄𝙀𝙉𝙀 𝙋𝙀𝙍𝙈𝙄𝙏𝙄𝘿𝙊 𝙐𝙎𝘼𝙍𝙈𝙀*')
 }
 handler.help = ['unbanchat']
 handler.tags = ['owner']
