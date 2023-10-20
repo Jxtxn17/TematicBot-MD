3398cad2de61fc91af6a47da1ddd6363a5834332
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2023-10-19 20:54:39 -0500

    Actualizar info-bot.js

--- plugins/info-bot.js
+++ plugins/info-bot.js
@@ -1,7 +1,7 @@
 import fs from "fs"
 let handler = m => m
 handler.all = async function (m) {
-let bot = `*🚀 Hola soy Bot*\nEn que te puedo ayudar?`
+let bot = `*🚀 𝙃𝙤𝙡𝙖 𝙨𝙤𝙮 𝙏𝙪𝙧𝙗𝙤*\n𝙀𝙣 𝙦𝙪𝙚 𝙩𝙚 𝙥𝙪𝙚𝙙𝙤 𝙖𝙮𝙪𝙙𝙖𝙧?`
 let vn = [imagen1, imagen2, imagen3, imagen4, img1].getRandom()
 let chat = global.db.data.chats[m.chat]
 if (/^bot$/i.test(m.text) && !chat.isBanned) { 
