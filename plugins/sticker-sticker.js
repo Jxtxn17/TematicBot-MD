2aaedee5c5cb01b6b09556a684d15f32976dabd6
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2023-10-22 10:18:30 -0500

    Actualizar sticker-sticker.js

--- plugins/sticker-sticker.js
+++ plugins/sticker-sticker.js
@@ -1,48 +1,71 @@
-import {sticker} from '../lib/sticker.js'; 
- import uploadFile from '../lib/uploadFile.js'; 
- import uploadImage from '../lib/uploadImage.js'; 
- import {webp2png} from '../lib/webp2mp4.js'; 
- const handler = async (m, {conn, args, usedPrefix, command}) => { 
-   if (usedPrefix == 'a' || usedPrefix == 'A') return; 
+import {sticker} from "../lib/sticker.js"; 
+ import uploadFile from "../lib/uploadFile.js"; 
+ import uploadImage from "../lib/uploadImage.js"; 
+ import {webp2png} from "../lib/webp2mp4.js"; 
+  
+ let handler = async (m, {conn, args, usedPrefix, command}) => { 
    let stiker = false; 
+   let username = conn.getName(m.sender); 
    try { 
-     const q = m.quoted ? m.quoted : m; 
-     const mime = (q.msg || q).mimetype || q.mediaType || ''; 
+     let q = m.quoted ? m.quoted : m; 
+     let mime = (q.msg || q).mimetype || q.mediaType || ""; 
      if (/webp|image|video/g.test(mime)) { 
-       if (/video/g.test(mime)) if ((q.msg || q).seconds > 10) return m.reply('*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻 𝚅𝙸𝙳𝙴𝙾 𝙽𝙾 𝙿𝚄𝙴𝙳𝙴 𝙳𝚄𝚁𝙰𝚁 𝙼𝙰𝚂 𝙳𝙴 10 𝚂𝙴𝙶𝚄𝙽𝙳𝙾𝚂*'); 
-       const img = await q.download?.(); 
-       if (!img) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙴 𝙰 𝚄𝙽 𝚅𝙸𝙳𝙴𝙾, 𝙸𝙼𝙰𝙶𝙴𝙽 𝙾 𝙸𝙽𝚂𝙴𝚁𝚃𝙴 𝙴𝙻 𝙴𝙽𝙻𝙰𝙲𝙴 𝙳𝙴 𝚄𝙽𝙰 𝙸𝙼𝙰𝙶𝙴𝙽 𝚃𝙴𝚁𝙼𝙸𝙽𝙰𝙲𝙸𝙾́𝙽 .𝚓𝚙𝚐 𝙴𝙻 𝙲𝚄𝙰𝙻 𝚂𝙴𝚁𝙰 𝙲𝙾𝙽𝚅𝙴𝚁𝚃𝙸𝙳𝙾 𝙴𝙽 𝚂𝚃𝙸𝙲𝙺𝙴𝚁, 𝙳𝙴𝙱𝙴 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙴𝚁 𝙾 𝚄𝚂𝙰𝚁 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 ${usedPrefix + command}*`; 
+       if (/video/g.test(mime)) if ((q.msg || q).seconds > 11) return m.reply("⚠️ *_Máximo 10 segundos._*"); 
+       let img = await q.download?.(); 
+       if (!img) 
+         throw `*[❗𝐈𝐍𝐅𝐎❗] 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙴 𝙰 𝚄𝙽 𝚅𝙸𝙳𝙴𝙾 𝙾 𝙸𝙼𝙰𝙶𝙴𝙽${usedPrefix + command}*`; 
        let out; 
        try { 
          stiker = await sticker(img, false, global.packname, global.author); 
        } catch (e) { 
          console.error(e); 
        } finally { 
+         await m.reply(wait); 
          if (!stiker) { 
            if (/webp/g.test(mime)) out = await webp2png(img); 
            else if (/image/g.test(mime)) out = await uploadImage(img); 
            else if (/video/g.test(mime)) out = await uploadFile(img); 
-           if (typeof out !== 'string') out = await uploadImage(img); 
+           if (typeof out !== "string") out = await uploadImage(img); 
            stiker = await sticker(false, out, global.packname, global.author); 
          } 
        } 
      } else if (args[0]) { 
        if (isUrl(args[0])) stiker = await sticker(false, args[0], global.packname, global.author); 
-       else return m.reply('*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻 𝙴𝙽𝙻𝙰𝙲𝙴 / 𝚄𝚁𝙻 / 𝙻𝙸𝙽𝙺 𝙽𝙾 𝙴𝚂 𝚅𝙰𝙻𝙸𝙳𝙰, 𝙻𝙰 𝚃𝙴𝚁𝙼𝙸𝙽𝙰𝙲𝙸𝙾𝙽 𝙳𝙴𝙻 𝙴𝙽𝙻𝙰𝙲𝙴 / 𝚄𝚁𝙻 / 𝙻𝙸𝙽𝙺 𝙳𝙴𝙱𝙴 𝚂𝙴𝚁 .𝚓𝚙𝚐, 𝙴𝙹𝙴𝙼𝙿𝙻𝙾: #s https://telegra.ph/file/0dc687c61410765e98de2.jpg*'); 
+       else return m.reply("URL invalido"); 
      } 
    } catch (e) { 
      console.error(e); 
      if (!stiker) stiker = e; 
    } finally { 
-     if (stiker) conn.sendFile(m.chat, stiker, 'sticker.webp', '', m); 
-     else throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙻𝙾 𝚂𝙸𝙴𝙽𝚃𝙾, 𝙾𝙲𝚄𝚁𝚁𝙸𝙾 𝚄𝙽 𝙴𝚁𝚁𝙾𝚁, 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝙽𝚃𝙰𝚁𝙻𝙾. 𝙽𝙾 𝙾𝙻𝚅𝙸𝙳𝙴 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙴 𝙰 𝚄𝙽 𝚅𝙸𝙳𝙴𝙾, 𝙸𝙼𝙰𝙶𝙴𝙽 𝙾 𝙸𝙽𝚂𝙴𝚁𝚃𝙴 𝙴𝙻 𝙴𝙽𝙻𝙰𝙲𝙴 𝙳𝙴 𝚄𝙽𝙰 𝙸𝙼𝙰𝙶𝙴𝙽 𝚃𝙴𝚁𝙼𝙸𝙽𝙰𝙲𝙸𝙾́𝙽 .𝚓𝚙𝚐 𝙴𝙻 𝙲𝚄𝙰𝙻 𝚂𝙴𝚁𝙰 𝙲𝙾𝙽𝚅𝙴𝚁𝚃𝙸𝙳𝙾 𝙴𝙽 𝚂𝚃𝙸𝙲𝙺𝙴𝚁*'; 
+     if (stiker) 
+       conn.sendFile( 
+         m.chat, 
+         stiker, 
+         "sticker.webp", 
+         "", 
+         m, 
+         true, 
+         { 
+           contextInfo: { 
+             forwardingScore: 200, 
+             isForwarded: false, 
+             externalAdReply: {showAdAttribution: false, title: wm, body: `h`, mediaType: 2, sourceUrl: gp1, thumbnail: imagen7}, 
+           }, 
+         }, 
+         {quoted: m} 
+       ); 
+     else 
+       throw "*[❗𝐈𝐍𝐅𝐎❗] 𝙾𝙲𝚄𝚁𝚁𝙸𝙾 𝚄𝙽 𝙴𝚁𝚁𝙾𝚁, 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝙽𝚃𝙰𝚁𝙻𝙾. 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙴 𝙰 𝚄𝙽 𝚅𝙸𝙳𝙴𝙾, 𝙸𝙼𝙰𝙶𝙴𝙽*"; 
    } 
  }; 
- handler.help = ['stiker (caption|reply media)', 'stiker <url>', 'stikergif (caption|reply media)', 'stikergif <url>']; 
- handler.tags = ['sticker']; 
- handler.command = /^(sfull|s2|sticker2|stickergif2|stickerwm2|stiker2)$/i; 
+ handler.help = ["sticker"]; 
+ handler.tags = ["sticker"]; 
+ handler.command = ["s", "sticker", "stiker"]; 
+  
  export default handler; 
   
  const isUrl = (text) => { 
-   return text.match(new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)(jpe?g|gif|png)/, 'gi')); 
+   return text.match( 
+     new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)(jpe?g|gif|png)/, "gi") 
+   ); 
  };
