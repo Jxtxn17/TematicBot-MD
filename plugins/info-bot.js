a2e5322fc0dfa904ce47300beeeaf1edc36a5724
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2023-10-28 11:50:37 -0500

    Actualizar info-bot.js

--- plugins/info-bot.js
+++ plugins/info-bot.js
@@ -1,12 +1,12 @@
-import fs from "fs" 
- let handler = m => m 
- handler.all = async function (m) { 
- let bot = `*🚀 Hola soy TurboBot*\nEn que te puedo ayudar?` 
- let vn = [imagen2].getRandom() 
- let chat = global.db.data.chats[m.chat] 
- if (/^bot$/i.test(m.text) && !chat.isBanned) {  
- conn.sendPresenceUpdate('recording', m.chat)     
- conn.sendFile(m.chat, vn, 'bot.jpg', bot, m, true, { type: 'conversation', ptt: true, sendEphemeral: true, quoted: estilo })} 
- return !0 
- } 
- export default handler
+import fs from "fs"
+let handler = m => m
+handler.all = async function (m) {
+let bot = `*🚀 Hola soy TurboBot*\nEn que te puedo ayudar?`
+let vn = [imagen1 img1].getRandom()
+let chat = global.db.data.chats[m.chat]
+if (/^bot$/i.test(m.text) && !chat.isBanned) { 
+conn.sendPresenceUpdate('recording', m.chat)    
+conn.sendFile(m.chat, vn, 'bot.jpg', bot, m, true, { type: 'conversation', ptt: true, sendEphemeral: true, quoted: estilo })}
+return !0
+}
+export default handler
