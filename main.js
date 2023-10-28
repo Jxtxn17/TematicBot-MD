766400323f3f8ee379e09786c37c638b47fcbe53
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2023-10-28 11:35:21 -0500

    Actualizar main.js

--- main.js
+++ main.js
@@ -126,7 +126,7 @@ auth: {
 creds: state.creds,
 keys: makeCacheableSignalKeyStore(state.keys, pino({level: 'silent'})),
 },
-browser: ['CuriosityBot-MD', 'Edge', '1.0.0'],
+browser: ['TurboBot-MD', 'Edge', '1.0.0'],
 version,
 defaultQueryTimeoutMs: undefined,
 }
