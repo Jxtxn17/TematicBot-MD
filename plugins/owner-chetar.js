6cca55c37127c8d4f20f322dfaf40f0ad459e62c
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2023-11-01 11:16:44 -0500

    Actualizar owner-chetar.js

--- plugins/owner-chetar.js
+++ plugins/owner-chetar.js
@@ -1,6 +1,6 @@
 const handler = async (m, { conn }) => { 
      const user = global.db.data.users[m.sender]; 
-         conn.sendMessage(m.chat, {text: `[🍬] @${m.sender.split('@')[0]} 𝙔𝙖 𝙣𝙤𝙩𝙚𝙣𝙙𝙧𝙖𝙨𝙡𝙞𝙢𝙞𝙩𝙚𝙨𝙢𝙞𝙘𝙧𝙚𝙖𝙙𝙤𝙧.`, mentions: [m.sender]}, {quoted: m}); 
+         conn.sendMessage(m.chat, {text: `[🍬] @${m.sender.split('@')[0]} 𝙈𝙞 𝙘𝙧𝙚𝙖𝙙𝙤𝙧, 𝙮𝙖 𝙣𝙤 𝙩𝙚𝙣𝙙𝙧𝙖𝙨 𝙡𝙞𝙢𝙞𝙩𝙚𝙨..`, mentions: [m.sender]}, {quoted: m}); 
        global.db.data.users[m.sender].money = Infinity; 
      global.db.data.users[m.sender].limit = Infinity; 
    global.db.data.users[m.sender].level = Infinity; 
