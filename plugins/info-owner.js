7035882f2cae99a49c4734ed39610f4e749d4d42
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2023-11-01 10:18:10 -0500

    Actualizar info-owner.js

--- plugins/info-owner.js
+++ plugins/info-owner.js
@@ -1,11 +1,11 @@
-function handler(m) {
-  const data = global.owner.filter(([id, isCreator]) => id && isCreator)
-  this.sendContact(m.chat, data.map(([id, name]) => [id, name]), estilo, m)
-
-}
-
-handler.help = ['owner']
-handler.tags = ['main']
-handler.command = ['owner', 'creator', 'creador', 'dueño'] 
-
-export default handler
+function handler(m) { 
+   const data = global.owner.filter(([id, isCreator]) => id && isCreator) 
+   this.sendContact(m.chat, data.map(([id, name]) => [id, name]), estilo, m) 
+  
+ } 
+  
+ handler.help = ['owner'] 
+ handler.tags = ['main'] 
+ handler.command = ['owner', 'creator', 'creador', 'dueño']  
+  
+ export default handler
