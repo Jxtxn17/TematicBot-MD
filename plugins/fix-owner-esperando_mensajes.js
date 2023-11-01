c0ac74f79e0e9d5b8943d035d458f0dbf1171a3c
@Azami <141449338+AzamiJs@users.noreply.github.com>
2023-11-01 11:18:12 -0600

    Update fix-owner-esperando_mensajes.js

--- plugins/fix-owner-esperando_mensajes.js
+++ plugins/fix-owner-esperando_mensajes.js
@@ -8,10 +8,10 @@
      return conn.sendMessage(m.chat, {text: '*[❗] Utiliza este comando directamente en el número principal del Bot.*'}, {quoted: m}); 
    } 
    await conn.sendMessage(m.chat, {text: '*[❗] Iniciando proceso de eliminación de todos los archivos de sesión, excepto el archivo creds.json...*'}, {quoted: m}); 
-   const sessionPath = './LoboSession/'; 
+   const sessionPath = './sessions/'; 
    try { 
      if (!existsSync(sessionPath)) { 
-       return await conn.sendMessage(m.chat, {text: '*[❗] La carpeta HachikoSession no existe o está vacía.*'}, {quoted: m}); 
+       return await conn.sendMessage(m.chat, {text: '*[❗] La carpeta sessions no existe o está vacía.*'}, {quoted: m}); 
      } 
      const files = await fs.readdir(sessionPath); 
      let filesDeleted = 0; 
@@ -22,7 +22,7 @@
        } 
      } 
      if (filesDeleted === 0) { 
-       await conn.sendMessage(m.chat, {text: '*[❗] No se encontró ningún archivo para eliminar en la carpeta LoboSession.*'}, {quoted: m}); 
+       await conn.sendMessage(m.chat, {text: '*[❗] No se encontró ningún archivo para eliminar en la carpeta sessions.*'}, {quoted: m}); 
      } else { 
        await conn.sendMessage(m.chat, {text: `*[❗] Se eliminaron ${filesDeleted} archivos de sesión, excepto el archivo creds.json.*`}, {quoted: m}); 
      } 
