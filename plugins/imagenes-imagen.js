334b43789d5e4c25ab8a9d8343af027a9cbb22b9
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2023-11-04 11:21:10 -0500

    Actualizar imagenes-imagen.js

--- plugins/imagenes-imagen.js
+++ plugins/imagenes-imagen.js
@@ -1,7 +1,7 @@
 import { googleImage } from '@bochilteam/scraper'
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
-if (!text) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙄𝙉𝙂𝙍𝙀𝙎𝙀 𝙐𝙉 𝙏𝙀𝙓𝙏𝙊*\n\n💡 𝙀𝙅𝙀𝙈𝙋𝙇𝙊\n*${usedPrefix + command} Curiosity*`
+if (!text) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙄𝙉𝙂𝙍𝙀𝙎𝙀 𝙐𝙉 𝙏𝙀𝙓𝙏𝙊*\n\n💡 𝙀𝙅𝙀𝙈𝙋𝙇𝙊\n*${usedPrefix + command} Turbo*`
 m.react(done)
 const res = await googleImage(text)
 let image = res.getRandom()
