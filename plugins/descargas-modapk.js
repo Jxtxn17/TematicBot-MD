781d91f25d858de82fb82945c48cf1b2b34153c4
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2023-11-11 14:26:40 -0500

    Create descargas-modapk.js

---
+++ plugins/descargas-modapk.js
@@ -0,0 +1,12 @@
+let response = `${eg}┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈\n┃💫 𝙉𝙊𝙈𝘽𝙍𝙀: ${data5.name}\n┃📦 𝙋𝘼𝘾𝙆𝘼𝙂𝙀: ${data5.package}\n┃🕒 𝙐𝙇𝙏𝙄𝙈𝘼 𝘼𝘾𝙏𝙐𝙇𝙄𝙕𝘼𝘾𝙄𝙊𝙉: ${data5.lastup}\n┃💪 𝙋𝙀𝙎𝙊 |  𝙎𝙄𝙕𝙀: ${data5.size}\n┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈\n┃ 𝘿𝙀𝙎𝘾𝘼𝙍𝙂𝘼𝙉𝘿𝙊 𝘼𝙋𝙆𝙎 🚀🚀🚀` 
+ await conn.sendMessage(m.chat, {image: {url: data5.icon}, caption: response}, {quoted: m}); 
+ if (data5.size.includes('GB') || data5.size.replace(' MB', '') > 999) { 
+ return await conn.sendMessage(m.chat, {text: '*EL APK ES MUY PESADO.*'}, {quoted: m})} 
+ await conn.sendMessage(m.chat, {document: {url: data5.dllink}, mimetype: 'application/vnd.android.package-archive', fileName: data5.name + '.apk', caption: null}, {quoted: m}); 
+ } catch { 
+ throw `${lenguajeGB['smsAvisoFG']()} *NO HAY RESULTADOS DE LO QUE SOLICITA*`; 
+ }}; 
+ handler.command = /^(apkmod|apk|modapk|dapk2|aptoide|aptoidedl)$/i; 
+ handler.register = true 
+ handler.limit = 2 
+ export default handler;
