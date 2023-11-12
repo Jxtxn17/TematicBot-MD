7af4b5475bf3b6d7b61cedbcc6753548a3358e9a
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2023-11-12 00:38:11 -0500

    Actualizar info-donar.js

--- plugins/info-donar.js
+++ plugins/info-donar.js
@@ -2,13 +2,13 @@ let media = './storage/logos/Menu2.jpg'
 let handler = async (m, { conn, command }) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 let str = `*「 🌟 DONAR 🌟 」*
----------------------
+╍╍┅╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍
 💙 *PAYPAL*
-🩵 *${paypal}*
----------------------
+ *${paypal}*
+╍┅┅┅┅┅┅┅╍╍╍╍╍┅╍┅╍╍
 👑 *Creador del bot*
-💬 wa.me/51929972576`
-
+wa.me/51929972576
+╍╍┅┅┅┅┅┅┅╍╍╍╍╍╍╍╍╍`
 await conn.sendFile(m.chat, media, 'Curiosity.jpg', str, fkontak)}
 handler.tags = ['main']
 handler.command = /^donar|donate|donasi$/i
