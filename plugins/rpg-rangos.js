6fba7af869a551d81916e3e56eacf9c0e521244a
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2023-11-23 12:28:25 -0500

    Actualizar rpg-rangos.js

--- plugins/rpg-rangos.js
+++ plugins/rpg-rangos.js
@@ -71,3 +71,8 @@ const role = [
 return role.reverse().find(role => level >= role.level)
 }}
 
+handler.help = ['daily'] 
+ handler.tags = ['xp'] 
+ handler.command = ['rangos', 'rango']  
+ handler.register = true 
+ export default handler
