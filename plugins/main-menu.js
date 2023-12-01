f57e20784f6f1f0f173327e11e87b0532778cb54
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2023-12-01 17:46:38 -0500

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -167,7 +167,8 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
 }
 handler.help = ['help']
 handler.tags = ['main']
-handler.command = ['help', 'menucompleto'] 
+handler.command = ['help',
+'menucompleto', 'allmenu'] 
 handler.register = true
 
 handler.exp = 3
