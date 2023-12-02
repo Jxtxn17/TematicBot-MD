6403715874693108a45c96e6a0c97b01e008ef99
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2023-12-02 15:23:55 -0500

    Actualizar menu-menu.js

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -6,9 +6,9 @@ let media = './storage/logos/Menu2.jpg'
 const ftrol = { key : { remoteJid: 'status@broadcast', participant : '0@s.whatsapp.net' }, message: { orderMessage: { itemCount : 2023, status: 1, surface : 1, message: `${name}!`, orderTitle: `▮Menu ▸`, sellerJid: '0@s.whatsapp.net' }}} 
  const fload = { key : { remoteJid: 'status@broadcast', participant : '0@s.whatsapp.net' }, message: { orderMessage: { itemCount : -99999999, status: 1, surface : 1, message: `CuriosityBot-MD 🍧` + `\nAzami`, orderTitle: `▮Menu ▸`, thumbnail: await (await fetch(pp)).buffer(), sellerJid: '0@s.whatsapp.net' }}} 
  
- let str = m.react('✅') 
   
- let menu = `*👋 Hola, ${name}* 
+ let menu = m.reaction (✅)
+ `*👋 Hola, ${name}* 
   
  *_${saludo}_* 
   
