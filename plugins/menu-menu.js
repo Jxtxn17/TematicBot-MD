1076b508197aa82b8256094b80982f1c5930f467
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2023-12-02 15:19:29 -0500

    Actualizar menu-menu.js

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -1,14 +1,19 @@
 let media = './storage/logos/Menu2.jpg' 
  let handler = async (m, { conn, command }) => { 
- let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" } 
- let str = `*「 🌟 DONAR 🌟 」* 
- ╍╍┅╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍ 
- 💙 *PAYPAL* 
-  *${paypal}* 
- ╍┅┅┅┅┅┅┅╍╍╍╍╍┅╍┅╍╍ 
- 👑 *Creador del bot* 
- wa.me/51929972576 
- ╍╍┅┅┅┅┅┅┅╍╍╍╍╍╍╍╍╍` 
+ let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
+
+//fakes
+const ftrol = { key : { remoteJid: 'status@broadcast', participant : '0@s.whatsapp.net' }, message: { orderMessage: { itemCount : 2023, status: 1, surface : 1, message: `${name}!`, orderTitle: `▮Menu ▸`, sellerJid: '0@s.whatsapp.net' }}} 
+ const fload = { key : { remoteJid: 'status@broadcast', participant : '0@s.whatsapp.net' }, message: { orderMessage: { itemCount : -99999999, status: 1, surface : 1, message: `CuriosityBot-MD 🍧` + `\nAzami`, orderTitle: `▮Menu ▸`, thumbnail: await (await fetch(pp)).buffer(), sellerJid: '0@s.whatsapp.net' }}} 
+ 
+ let str = m.react('✅') 
+  
+ let menu = `*👋 Hola, ${name}* 
+  
+ *_${saludo}_* 
+  
+ .allmenu 
+ _(Para ver el menú completo)_` 
  await conn.sendFile(m.chat, media, 'Curiosity.jpg', str, fkontak)} 
  handler.tags = ['main'] 
  handler.command = /^menu$/
