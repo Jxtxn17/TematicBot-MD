f6c0257f036a606c240832879c1db985534acb30
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2023-12-03 13:03:25 -0500

    Actualizar fix-esperando_mensaje.js

--- plugins/fix-esperando_mensaje.js
+++ plugins/fix-esperando_mensaje.js
@@ -8,7 +8,7 @@
      return conn.sendMessage(m.chat, {text: '*[❗] Utiliza este comando directamente en el número principal del Bot*'}, {quoted: m}); 
    } 
    const chatId = m.isGroup ? [m.chat, m.sender] : [m.sender]; 
-   const sessionPath = './TurboSession/'; 
+   const sessionPath = './BaileySession/'; 
    try { 
      const files = await fs.readdir(sessionPath); 
      let filesDeleted = 0; 
