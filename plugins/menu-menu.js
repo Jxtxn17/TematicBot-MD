fdc0be7f57c2a0a69b5db0ce7bfba14de4832353
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2023-12-02 21:41:00 -0500

    Actualizar menu-menu.js

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -1,6 +1,4 @@
-let media = './storage/logos/Menu2.jpg' 
- let handler = async (m, { conn, command }) => { 
- let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
+let media = './storage/logos/Menu2.jpg'
 
 //fakes
 const ftrol = { key : { remoteJid: 'status@broadcast', participant : '0@s.whatsapp.net' }, message: { orderMessage: { itemCount : 2023, status: 1, surface : 1, message: `!`, orderTitle: `▮Menu ▸`, sellerJid: '0@s.whatsapp.net' }}} 
