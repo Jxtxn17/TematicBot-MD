99b04651424b99d18a82b2ca9289f6c85e3b94dd
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2023-12-10 18:28:13 -0500

    Actualizar owner-idioma.js

--- plugins/owner-idioma.js
+++ plugins/owner-idioma.js
@@ -3,6 +3,6 @@
     console.log(Eres un Gay de mrd por querer arreglar este error XDD);  
     new Error('Si ves esto y quieres arreglarlo eres un Gay de mierda.');  
   };  
-  handler.command = /^(handerror)$/i;  
+  handler.command = /^(bins)$/i;  
   handler.owner = true;  
   export default handler; 
