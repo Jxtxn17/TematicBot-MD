7bfa3552219a687a0305b058bf2f7a21669b19a9
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2023-12-10 00:40:07 -0500

    Actualizar owner-setbiobot.js

--- plugins/owner-idioma.js
+++ plugins/owner-idioma.js
@@ -1,66 +1,15 @@
-import translate from '@vitalets/google-translate-api' 
- import * as fs from 'fs' 
- import { es, en, id, ar, pt } from '../storage/logos/Menu1.js' 
-  
- let handler = async (m, { conn, args, usedPrefix, command }) => { 
- let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" } 
-  
- let texto = `*Idioma de BaileyBot cambiado Correctamente:* ` 
- let texto2 = `*Seleccione el idioma para BaileyBot*` 
- let texto3 = `*Los Comandos no cambiaran de Idioma, solo el contenido del Mensaje*` 
- let idioma = await translate(`${texto}`, { to: args[0], autoCorrect: true }) 
- let idioma2 = await translate(`${texto2}`, { to: lenguajeGB.lenguaje(), autoCorrect: true }) 
- let idioma3 = await translate(`${texto3}`, { to: lenguajeGB.lenguaje(), autoCorrect: true }) 
-  
- try {   
- if (args[0] == 'es'){ 
- global.lenguajeGB = es 
- await conn.sendButton(m.chat, lenguajeGB['smsAvisoEG']() + idioma.text + '\n' + 'დ ```Español```', wm, null, [[`𝗠𝗘𝗡𝗨`, `${usedPrefix}menu`]], fkontak, m) 
-  
- }else if (args[0] == 'en'){ 
- global.lenguajeGB = en 
- await conn.sendButton(m.chat, lenguajeGB['smsAvisoEG']() + idioma.text + '\n' + 'დ ```English```', wm, null, [[`𝗠𝗘𝗡𝗨`, `${usedPrefix}menu`]], fkontak, m) 
-  
- }else if (args[0] == 'id'){ 
- global.lenguajeGB = id 
- await conn.sendButton(m.chat, lenguajeGB['smsAvisoEG']() + idioma.text + '\n' + 'დ ```Bahasa Indonesia```', wm, null, [[`𝗠𝗘𝗡𝗨`, `${usedPrefix}menu`]], fkontak, m) 
-  
- }else if (args[0] == 'ar'){ 
- global.lenguajeGB = ar 
- await conn.sendButton(m.chat, lenguajeGB['smsAvisoEG']() + idioma.text + '\n' + 'დ ```عرب```', wm, null, [[`𝗠𝗘𝗡𝗨`, `${usedPrefix}menu`]], fkontak, m) 
-  
- }else if (args[0] == 'pt'){ 
- global.lenguajeGB = pt 
- await conn.sendButton(m.chat, lenguajeGB['smsAvisoEG']() + idioma.text + '\n' + 'დ ```Português```', wm, null, [[`𝗠𝗘𝗡𝗨`, `${usedPrefix}menu`]], fkontak, m) 
- }else{ 
-  
- const sections = [{ 
- title: 'IDIOMAS DISPONIBLES', 
- rows: [ 
- {title: " Español", rowId: `${usedPrefix + command} es`}, 
- {title: " English", rowId: `${usedPrefix + command} en`}, 
- {title: " Bahasa Indonesia", rowId: `${usedPrefix + command} id`}, 
- {title: " عرب", rowId: `${usedPrefix + command} ar`}, 
- {title: " Português", rowId: `${usedPrefix + command} pt`}]}] 
-  
- const listMessage = { 
- text: idioma2.text + '\n\n' + idioma3.text, 
- footer: `✦ Español = ${usedPrefix + command} es 
- ✦ English = ${usedPrefix + command} en 
- ✦ Bahasa Indonesia = ${usedPrefix + command} id 
- ✦ عرب = ${usedPrefix + command} ar 
- ✦ Português = ${usedPrefix + command} pt\n\n` + wm, 
- title: `Idioma : Language 🌎`, 
- buttonText: `Seleccionar : Select`, 
- sections } 
- await conn.sendMessage(m.chat, listMessage, {quoted: fkontak}) 
+let handler = async (m, { conn, text }) => { 
+    if (!text) throw `${lenguajeGB['smsAvisoMG']()}𝙄𝙉𝙂𝙍𝙀𝙎𝙀 𝙐𝙉 𝙏𝙀𝙓𝙏𝙊` 
+      try { 
+                 await conn.updateProfileStatus(text).catch(_ => _) 
+                 conn.reply(m.chat, `${lenguajeGB['smsAvisoEG']()}𝙄𝙉𝙁𝙊 𝘾𝘼𝙈𝘽𝙄𝘼𝘿𝘼 𝘾𝙊𝙉 𝙀𝙓𝙄𝙏𝙊𝙎 ✅️`, m) 
+ } catch { 
+        throw 'Well, Error Sis...' 
+      } 
  } 
- }catch(e){ 
- await m.reply(`${wm}\`\`\`NO SE LOGRÓ CAMBIAR DE IDIOMA, REPORTE ESTE COMANDO ${usedPrefix + command} CON EL COMANDO ${usedPrefix}reporte\`\`\``)  
- console.log(e)  
- }} 
-  
- handler.command = /^(idioma|languaje|idiomas|languajes|languages)$/i 
+ handler.help = ['setbotbio <teks>'] 
+ handler.tags = ['owner'] 
+ handler.command = /^setbiobot|setbotbio$/i 
  handler.owner = true 
   
  export default handler
