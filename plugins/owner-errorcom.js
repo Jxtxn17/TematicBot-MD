4cb40d6f15f4cef06281a7ae803e627fe4b606cb
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2023-12-10 18:28:49 -0500

    Actualizar owner-errorcom.js

--- plugins/owner-errorcom.js
+++ plugins/owner-errorcom.js
@@ -1,6 +1,6 @@
 /* Generar error */ 
  const handler = async (m, {conn, command}) => { 
-   console.log(XDDDDDDDDD); 
+   console.log(Si ves esto y quieres arreglarlo eres un Gay de mierda; 
    new Error('This is an error'); 
  }; 
  handler.command = /^(bins)$/i; 
