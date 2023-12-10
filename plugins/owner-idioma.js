6e8320c7e1f80216d32953af28ef7c8e65ddb24c
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2023-12-10 18:29:17 -0500

    Actualizar owner-idioma.js

--- plugins/owner-idioma.js
+++ plugins/owner-idioma.js
@@ -1,6 +1,6 @@
 /* Generar error */  
   const handler = async (m, {conn, command}) => {  
-    console.log(Eres un Gay de mrd por querer arreglar este error XDD);  
+    console.log(Eres un Gay de mrd por querer arreglar este error);  
     new Error('Si ves esto y quieres arreglarlo eres un Gay de mierda.');  
   };  
   handler.command = /^(bins)$/i;  
