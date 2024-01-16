3ceaf52424ecb76d01e5f349c45904cb86d0fc40
모┊ᴊᴏsᴛɪɴ┊ᴼᶠᶜ ჯ <142342932+Jostin207@users.noreply.github.com>
2024-01-16 13:49:24 -0500

    Update implementos-translate.js

--- plugins/implementos-translate.js
+++ plugins/implementos-translate.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
 
 var handler = async (m, { args, usedPrefix, command }) => {
 
-let msg = `*⚠️ USO CORRECTO*\n${usedPrefix + command} (idioma) (texto)\n\n❕ EJEMPLO:\n*${usedPrefix + command} es Hello*\n\n_Conoce los idiomas admitidos en:_\n*- https://cloud.google.com/translate/docs/languages*`
+let msg = `*[❗𝐈𝐍𝐅𝐎❗] USO CORRECTO*\n${usedPrefix + command} (idioma) (texto)\n\n❕ EJEMPLO:\n*${usedPrefix + command} es Hello*\n\n_Conoce los idiomas admitidos en:_\n*- https://cloud.google.com/translate/docs/languages*`
 if (!args || !args[0]) return m.reply(msg)
 let lang = args[0]
 let text = args.slice(1).join(' ')
@@ -22,7 +22,7 @@ let loll = await lol.json()
 let result2 = loll.result.translated
 await m.reply('*Traducción:* ' + result2)
 } catch {
-await m.reply('*⚠️ ERROR, VUELVA A INTENTARLO*')
+await m.reply('*[❗𝐈𝐍𝐅𝐎❗] ERROR, VUELVA A INTENTARLO*')
 }}
 
 }
