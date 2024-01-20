010e87a716a97c12ca85dea4076c8c1e17146412
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2024-01-19 19:43:59 -0500

    Actualizar combinao.js

--- plugins/combinao.js
+++ plugins/combinao.js
@@ -1,12 +1,1 @@
-import fs from "fs" 
- let handler = m => m 
- handler.all = async function (m) { 
- let bot = `*Puro*\nPepon mi nto` 
- let vn = [imagen6, img1].getRandom() 
- let chat = global.db.data.chats[m.chat] 
- if (/^Pepon$/i.test(m.text) && !chat.isBanned) {  
- conn.sendPresenceUpdate('recording', m.chat)     
- conn.sendFile(m.chat, vn, 'bot.jpg', bot, m, true, { type: 'conversation', ptt: true, sendEphemeral: true, quoted: estilo })} 
- return !0 
- } 
- export default handler
+gay
