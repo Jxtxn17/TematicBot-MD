beb57a98585e0bbbcd19daf19483b03efa2cc084
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2024-02-07 14:50:20 -0500

    Actualizar bailey.sh

--- turbo.sh
+++ turbo.sh
@@ -2,7 +2,7 @@
 
 #!/data/data/com.termux/files/usr/bin/bash 
 # Interpretación determinada para la ejecución     
-COMANDOS = "pkg install git -y\npkg install nodejs -y\npkg install ffmpeg -y\npkg install imagemagick -y\npkg install -y yarn\ngit clone https://github.com/Jxtxn17/BaileyBot-MD\ncd TurboBot-MD\nyarn install\nnpm install\nnpm start"
+COMANDOS = "pkg install git -y\npkg install nodejs -y\npkg install ffmpeg -y\npkg install imagemagick -y\npkg install -y yarn\ngit clone https://github.com/Jxtxn17/BaileyBot-MD\ncd BaileyBot-MD\nyarn install\nnpm install\nnpm start"
 
 echo -e "\e[38;5;183m
 ─█▀▀█ ───░█ ░█─░█ ░█▀▀▀█ ▀▀█▀▀ ░█▀▀▀ 　 ░█─── ─█▀▀█ 　 ░█▀▀█ ─█▀▀█ ░█▄─░█ ▀▀█▀▀ ─█▀▀█ ░█─── ░█─── ─█▀▀█ 
