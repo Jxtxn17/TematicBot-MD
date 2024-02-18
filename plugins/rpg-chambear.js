f45d91bc3b4f946d023848be57a2a86a3aed58ee
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2024-02-17 23:14:04 -0500

    Update rpg-chambear.js

--- plugins/rpg-chambear.js
+++ plugins/rpg-chambear.js
@@ -1,8 +1,1 @@
-let handler = async (m, { conn, command }) => {
-let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
-let text = `Chambeaste y ahora ganaste un pene negro guasaa`
-await conn.sendFile(m.chat, media, 'Curiosity.jpg', str, fkontak)}
-handler.tags = ['main']
-handler.command = /^chamba$/i
-handler.register = true
-export default handler
+.
