31fee6fd6262a2e6feb4d13d755ee89c2f6c6aac
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2024-02-22 17:49:58 -0500

    Actualizar transformador-toimg.js

--- plugins/transformador-toimg.js
+++ plugins/transformador-toimg.js
@@ -1,21 +1,17 @@
 import { webp2png } from '../lib/webp2mp4.js'
 
-var handler = async (m, { conn, usedPrefix, command }) => {
-
-const notStickerMessage = `*⚠️ RESPONDA CON ${usedPrefix + command} AL STICKER QUE DESEE CONVERTIR EN IMAGEN*`
-if (!m.quoted) throw notStickerMessage 
-const q = m.quoted || m
-let mime = q.mediaType || ''
-if (!/sticker/.test(mime)) throw notStickerMessage
-let media = await q.download()
-let out = await webp2png(media).catch(_ => null) || Buffer.alloc(0)
-await conn.sendFile(m.chat, out, 'error.png', null, fkontak, m)
-
+let handler = async (m, { conn, usedPrefix, command }) => {
+    const notStickerMessage = `[❗] 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙴 𝙰 𝚄𝙽 𝚂𝚃𝙸𝙲𝙺𝙴𝚁 𝙲𝙾𝙽 :\n\n *${usedPrefix + command}*`
+    if (!m.quoted) throw notStickerMessage
+    const q = m.quoted || m
+    let mime = q.mediaType || ''
+    if (!/sticker/.test(mime)) throw notStickerMessage
+    let media = await q.download()
+    let out = await webp2png(media).catch(_ => null) || Buffer.alloc(0)
+    await conn.sendFile(m.chat, out, 'out.png', '*✅ 𝙰𝚀𝚄𝙸 𝚃𝙸𝙴𝙽𝙴𝚂*', m)
 }
-handler.help = ['toimg']
-handler.tags = ['transformador']
-handler.command = ['toimg', 'jpg', 'jpge', 'png']
-
-handler.limit = true
+handler.help = ['toimg <sticker>']
+handler.tags = ['sticker']
+handler.command = ['toimg', 'jpg', 'aimg'] 
 
 export default handler
