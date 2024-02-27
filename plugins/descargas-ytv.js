2b4722dbdeb4ced1b84d2d2459a65273a3630640
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2024-02-27 18:43:43 -0500

    Actualizar descargas-ytv.js

--- plugins/descargas-ytv.js
+++ plugins/descargas-ytv.js
@@ -1,9 +1,9 @@
 import { youtubedl, youtubedlv2 } from '@bochilteam/scraper'
 import fetch from 'node-fetch'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-if (!args[0]) return conn.reply(m.chat, `*⚠️ INGRESE UN ENLACE DE YOUTUBE PARA DESCARGAR EL VIDEO*\n\n❕ EJEMPLO\n*${usedPrefix + command} https://youtu.be/85xI8WFMIUY*`, fkontak, m)
+if (!args[0]) return conn.reply(m.chat, `*[❗𝐈𝐍𝐅𝐎❗] 𝙄𝙉𝙂𝙍𝙀𝙎𝙀 𝙐𝙉 𝙀𝙉𝙇𝘼𝘾𝙀 𝘿𝙀 𝙔𝙊𝙐𝙏𝙐𝘽𝙀 𝙋𝘼𝙍𝘼 𝘿𝙀𝙎𝘾𝘼𝙍𝙂𝘼𝙍 𝙀𝙇 𝙑𝙄𝘿𝙀𝙊*\n\n❕ 𝙀𝙅𝙀𝙈𝙋𝙇𝙊\n*${usedPrefix + command} https://youtu.be/85xI8WFMIUY*`, fkontak, m)
 m.react(rwait)
-await conn.reply(m.chat, `*🚀 SE ESTA DESCARGANDO SU VÍDEO, ESPERE UN MOMENTO*`, m)
+await conn.reply(m.chat, `*🚀 𝙎𝙀 𝙀𝙎𝙏𝘼 𝘿𝙀𝙎𝘾𝘼𝙍𝙂𝘼𝙉𝘿𝙊 𝙎𝙐 𝙑𝙄́𝘿𝙀𝙊, 𝙀𝙎𝙋𝙀𝙍𝙀 𝙐𝙉 𝙈𝙊𝙈𝙀𝙉𝙏𝙊*`, m)
 try {
 let qu = args[1] || '360'
 let q = qu + 'p'
@@ -12,7 +12,7 @@ const yt = await youtubedl(v).catch(async _ => await youtubedlv2(v))
 const dl_url = await yt.video[q].download()
 const ttl = await yt.title
 const size = await yt.video[q].fileSizeH
-await await conn.sendMessage(m.chat, { video: { url: dl_url }, fileName: `${ttl}.mp4`, mimetype: 'video/mp4', caption: `*📑 TÍTULO*\n${ttl}\n\n*📊 PESO*\n${size}`, thumbnail: await fetch(yt.thumbnail) }, { quoted: m })
+await await conn.sendMessage(m.chat, { video: { url: dl_url }, fileName: `${ttl}.mp4`, mimetype: 'video/mp4', caption: `*📑 𝙏𝙄́𝙏𝙐𝙇𝙊*\n${ttl}\n\n*📊 𝙋𝙀𝙎𝙊*\n${size}`, thumbnail: await fetch(yt.thumbnail) }, { quoted: m })
 } catch {
 try {
 let lolhuman = await fetch(`https://api.lolhuman.xyz/api/ytvideo2?apikey=${lolkeysapi}&url=${args[0]}`)    
@@ -22,9 +22,9 @@ let n2 = lolh.result.link
 let n3 = lolh.result.size
 let n4 = lolh.result.thumbnail
 m.react(done)
-await conn.sendMessage(m.chat, { video: { url: n2 }, fileName: `${n}.mp4`, mimetype: 'video/mp4', caption: `*📑 TÍTULO*\n${n}\n\n*📍 PESO*\n${n3}`, thumbnail: await fetch(n4) }, { quoted: m })
+await conn.sendMessage(m.chat, { video: { url: n2 }, fileName: `${n}.mp4`, mimetype: 'video/mp4', caption: `*📑 𝙏𝙄́𝙏𝙐𝙇𝙊*\n${n}\n\n*📍 𝙋𝙀𝙎𝙊*\n${n3}`, thumbnail: await fetch(n4) }, { quoted: m })
 } catch {
-await conn.reply(m.chat, `*⚠️ ES POSIBLE QUE EL VIDEO SEA MUY PESADO. INTENTE CON OTRA OPCIÓN DE DESCARGA*`, fkontak, m)
+await conn.reply(m.chat, `*[❗𝐈𝐍𝐅𝐎❗] 𝙀𝙎 𝙋𝙊𝙎𝙄𝘽𝙇𝙀 𝙌𝙐𝙀 𝙀𝙇 𝙑𝙄𝘿𝙀𝙊 𝙎𝙀𝘼 𝙈𝙐𝙔 𝙋𝙀𝙎𝘼𝘿𝙊. 𝙄𝙉𝙏𝙀𝙉𝙏𝙀 𝘾𝙊𝙉 𝙊𝙏𝙍𝘼 𝙊𝙋𝘾𝙄𝙊́𝙉 𝘿𝙀 𝘿𝙀𝙎𝘾𝘼𝙍𝙂𝘼*`, fkontak, m)
 m.react(error)}
 }
 
