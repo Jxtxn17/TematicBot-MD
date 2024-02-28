125673d9683ee3a3a0431b76534aded6cc304a7b
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2024-02-28 16:20:17 -0500

    Actualizar combinao.js

--- plugins/combinao.js
+++ plugins/combinao.js
@@ -7,15 +7,15 @@ let handler = async (m, { conn, text}) => {
 
 let hasil = Math.floor(Math.random() * 5000)
 
-conn.reply(m.chat, `*╭─────◈🌹◈──────╮*
+conn.reply(m.chat, `*╭─────◈🌹🗿◈──────╮*
 
-${pickRandom(global.piropo)}
+${pickRandom(global.chamba)}
 
-*╰─────◈🌹◈──────╯*`, m, { contextInfo:{ externalAdReply: {title: '🥀 Piropo 🥀', body: saludo, sourceUrl: global.paypal, thumbnail: rosas }}})
+*╰─────◈🗿◈──────╯*`, m, { contextInfo:{ externalAdReply: {title: '🗿 Chamba 🗿', body: saludo, sourceUrl: global.paypal, thumbnail: rosas }}})
 
 }
-handler.help = ['piropo']
-handler.tags = ['frases']
+handler.help = ['chambear']
+handler.tags = ['chamba']
 handler.command = ['chambear']
 handler.fail = null
 handler.exp = 0
@@ -40,4 +40,4 @@ function pickRandom(list) {
 return list[Math.floor(list.length * Math.random())]
 }
 
-global.piropo = ["*CHAMBEANDO..*\n\n*Estuviste chambeando y 3 negros se corrieron en tu boca..",
+global.chamba = ["*CHAMBEANDO..*\n\n*Estuviste chambeando y 3 negros se corrieron en tu boca..",
