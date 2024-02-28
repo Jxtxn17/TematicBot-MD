a398887fa3b7db3e8f39616da2fb4af7e8538c8e
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2024-02-28 15:54:37 -0500

    Actualizar combinao.js

---
+++ plugins/combinao.js
@@ -0,0 +1,16 @@
+var handler = async (m, {command, conn, args, usedPrefix, text}) => {
+  if (command == "y" || command == "y") {
+    let mp4 = `*_descargando :_
+_${usedPrefix}ytmp4_`.trim();
+
+    m.reply(mp4);
+  }
+
+  if (command == "cursos") {
+    var play = `*CHAMBEANDO..*\n\n*Estuviste Chambeando y 3 negros te llegaron y fuiste violado..`.trim();
+    m.reply(play);
+  }
+};
+handler.command = ["chamba", "chambear", ""];
+handler.tags = ["internet"];
+export default handler
