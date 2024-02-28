06576ae0f026a29100f0bbbc8927e9e347aff638
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2024-02-28 15:57:20 -0500

    Actualizar combinao.js

--- plugins/combinao.js
+++ plugins/combinao.js
@@ -7,7 +7,11 @@ _${usedPrefix}ytmp4_`.trim();
   }
 
   if (command == "chambear") {
-    var play = `*CHAMBEANDO..*\n\n*Estuviste Chambeando y 3 negros te llegaron y fuiste violado..`.trim();
+    var play = `*CHAMBEANDO..*\n\n*Estuviste Chambeando y 3 negros te llegaron y fuiste violado..*`.trim();
+    m.reply(play);
+
+if (command == "chambear") {
+    var play = `*CHAMBEANDO..*\n\n*Estuviste Chambeando y 5 negros se corrieron en tu boca..*`.trim();
     m.reply(play);
   }
 };
