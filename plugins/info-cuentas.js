366393eedc413bb754e621c0f529470615a622ac
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2024-02-29 14:55:46 -0500

    Actualizar info-cuentas.js

--- plugins/info-cuentas.js
+++ plugins/info-cuentas.js
@@ -3,14 +3,14 @@ let handler = async (m, { conn, command }) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 let str = `
 ┏───◆ ${wm} ◆───┓
-│ *GITHUB*
-│ ${git}
+ *GITHUB*
+ ${git}
 ┣────────────────
-│ *INSTAGRAM*
-│ ${ig}
+ *INSTAGRAM*
+ ${ig}
 ┣────────────────
-│ *YOUTUBE*
-│ ${yt}
+ *YOUTUBE*
+ ${yt}
 ┗───◆ ${wm} ◆───┛`
 
 await conn.sendFile(m.chat, media, 'gata.mp4', str, fkontak)}
