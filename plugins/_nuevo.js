0600a493206370106baf5edb79225a1eecb9f499
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2024-03-15 19:02:50 -0500

    Actualizar _nuevo.js

--- plugins/_nuevo.js
+++ plugins/_nuevo.js
@@ -1,1 +1,20 @@
-.
+import namso = from 'namso-cc-gen;
+
+const handler = async (m, {conn, text,command, usedPrefix}) => {
+let res = namso.gen({
+ ShowCCV: true,
+ CCV: 'rnd',
+ ShowExpDate: true,
+ ShowBank: false,
+ Month: "09",
+ Year: "2026",
+ Quantity: "10",
+ Bin: "{txt}",
+ Format: "PIPE"
+})
+return res;
+  }
+};
+
+handler.command = /^(bin|cc)$/i;
+export default handler;
