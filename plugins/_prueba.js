ec1ec00ee9dbd21a332f45e45124f838cce629a8
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2024-03-15 20:24:59 -0500

    Actualizar _prueba.js

--- plugins/_prueba.js
+++ plugins/_prueba.js
@@ -1,14 +1,1 @@
-const namso = require('namso-cc-gen');
-
-let res = namso.gen({
- ShowCCV: true,
- CCV: 'rnd',
- ShowExpDate: true,
- ShowBank: false,
- Month: "09",
- Year: "2026",
- Quantity: "10",
- Bin: "486742802285xxxx",
- Format: "PIPE"
-})
-return res
+..
