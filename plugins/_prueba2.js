a90483bff492b556870cdc95c91d0dcce68e9a4e
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2024-03-17 18:33:12 -0500

    Actualizar _prueba2.js

--- plugins/_prueba2.js
+++ plugins/_prueba2.js
@@ -12,15 +12,15 @@ try {
     const isParticipant = participants.some((u) => conn.decodeJid(u.id) === conn.user.jid);
     const participantStatus = isParticipant ? '🪪 𝚄𝙽𝙸𝙳𝙾' : '❌ 𝚂𝙰𝙻𝙸𝙳𝙾';
     const totalParticipants = participants.length;
-    txt += `*🚩 𝙶𝚁𝚄𝙿𝙾 ${i + 1}*
+    txt += `*📝 𝙶𝚁𝚄𝙿𝙾 ${i + 1}*
     *◉ 𝙽𝙾𝙼𝙱𝚁𝙴:* ${await conn.getName(jid)}
     *◉ 𝙸𝙳:* ${jid}
-    *◉ 𝙰𝙳𝙼𝙸𝙽:* ${isBotAdmin ? '✔ Sí' : '❌ No'}
+    *◉ 𝙰𝙳𝙼𝙸𝙽:* ${isBotAdmin ? '✅' : '❌'}
     *◉ 𝙴𝚂𝚃𝙰𝙳𝙾:* ${participantStatus}
     *◉ 𝚃𝙾𝚃𝙰𝙻 𝙳𝙴 𝙼𝙸𝙴𝙼𝙱𝚁𝙾𝚂:* ${totalParticipants}
-    *◉ 𝙴𝙽𝙻𝙰𝙲𝙴:* ${isBotAdmin ? `https://chat.whatsapp.com/${await conn.groupInviteCode(jid) || '❗ 𝙴𝚁𝚁𝙾𝚁'}` : '◉ 𝙽𝙾 𝚂𝙾𝚈 𝙰𝙳𝙼𝙸𝙽'}\n\n`;
+    *◉ 𝙴𝙽𝙻𝙰𝙲𝙴:* ${isBotAdmin ? `https://chat.whatsapp.com/${await conn.groupInviteCode(jid) || '❗ 𝙴𝚁𝚁𝙾𝚁'}` : '◉ 𝙽𝙾 𝚃𝙴𝙽𝙶𝙾 𝙰𝙳𝙼𝙸𝙽'}\n\n`;
   }
-  conn.reply(m.chat, `🚩 *𝙻𝙸𝚂𝚃𝙰 𝙳𝙴 𝙶𝚁𝚄𝙿𝙾𝚂*\n\n◉ *𝚃𝙾𝚃𝙰𝙻 𝙳𝙴 𝙶𝚁𝚄𝙿𝙾𝚂* ${totalGroups}
+  conn.reply(m.chat, `📝 *𝙻𝙸𝚂𝚃𝙰 𝙳𝙴 𝙶𝚁𝚄𝙿𝙾𝚂*\n\n◉ *𝚃𝙾𝚃𝙰𝙻 𝙳𝙴 𝙶𝚁𝚄𝙿𝙾𝚂* ${totalGroups}
 
 ${txt}`, m, fake, )
 } catch {
@@ -35,15 +35,15 @@ ${txt}`, m, fake, )
     const isParticipant = participants.some((u) => conn.decodeJid(u.id) === conn.user.jid);
     const participantStatus = isParticipant ? '🪪 𝚄𝙽𝙸𝙳𝙾' : '❌ 𝚂𝙰𝙻𝙸𝙳𝙾';
     const totalParticipants = participants.length;    
-    txt += `*🚩 𝙶𝚁𝚄𝙿𝙾 ${i + 1}*
+    txt += `*📝 𝙶𝚁𝚄𝙿𝙾 ${i + 1}*
     *◉ 𝙽𝙾𝙼𝙱𝚁𝙴:* ${await conn.getName(jid)}
     *◉ 𝙸𝙳:* ${jid}
-    *◉ 𝙰𝙳𝙼𝙸𝙽:* ${isBotAdmin ? '✔ 𝚂𝙸' : '❌ 𝙽𝙾'}
+    *◉ 𝙰𝙳𝙼𝙸𝙽:* ${isBotAdmin ? '✅' : '❌'}
     *◉ 𝙴𝚂𝚃𝙰𝙳𝙾:* ${participantStatus}
     *◉ 𝚃𝙾𝚃𝙰𝙻 𝙳𝙴 𝙼𝙸𝙴𝙼𝙱𝚁𝙾𝚂:* ${totalParticipants}
-    *◉ 𝙴𝙽𝙻𝙰𝙲𝙴:* ${isBotAdmin ? '❗ 𝙴𝚁𝚁𝙾𝚁' : '◉ 𝙽𝙾 𝚂𝙾𝚈 𝙰𝙳𝙼𝙸𝙽'}\n\n`;
+    *◉ 𝙴𝙽𝙻𝙰𝙲𝙴:* ${isBotAdmin ? '❗ 𝙴𝚁𝚁𝙾𝚁' : '◉ 𝙽𝙾 𝚃𝙴𝙽𝙶𝙾 𝙰𝙳𝙼𝙸𝙽'}\n\n`;
   }
- conn.reply(m.chat, `🚩 *𝙻𝙸𝚂𝚃𝙰 𝙳𝙴 𝙶𝚁𝚄𝙿𝙾𝚂*\n\n◉ *𝚃𝙾𝚃𝙰𝙻 𝙶𝚁𝚄𝙿𝙾𝚂:* ${totalGroups}
+ conn.reply(m.chat, `📝 *𝙻𝙸𝚂𝚃𝙰 𝙳𝙴 𝙶𝚁𝚄𝙿𝙾𝚂*\n\n◉ *𝚃𝙾𝚃𝙰𝙻 𝙶𝚁𝚄𝙿𝙾𝚂:* ${totalGroups}
 
 ${txt}`, m, fake, )
  }    
