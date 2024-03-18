0f42688de734068b7c3bb6a210402b91adee991e
matias-crypto <149725150+matias-crypto@users.noreply.github.com>
2024-03-18 02:43:22 -0300

    Create comando-prueba.js

---
+++ plugins/comando-prueba.js
@@ -0,0 +1,18 @@
+import fetch from 'node-fetch';
+const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems}) => {
+  try {
+  if (usedPrefix == 'a' || usedPrefix == 'A') return;
+
+  const date = d.toLocaleDateString(locale, {day: 'numeric', month: 'long', year: 'numeric'});
+  const {money, joincount} = global.db.data.users[m.sender];
+  const {exp, limit, level, role} = global.db.data.users[m.sender];
+  const pp = await conn.profilePictureUrl(conn.user.jid).catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png');
+  const fkon = { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: `status@broadcast` } : {}) }, message: { 'contactMessage': { 'displayName': wm, 'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:XL;${wm},;;;\nFN:${wm},\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabell:Ponsel\nEND:VCARD`, 'jpegThumbnail': imagen1, thumbnail: imagen1 ,sendEphemeral: true}}};
+    await conn.reply(m.chat, 'Hola humano, soy el mejor bot que puede existir, siga al creador en github', m, { contextInfo:{ forwardingScore: 2024, isForwarded: true, externalAdReply: {title: 'Hola!!', body: 'sɪɢᴜᴇᴍᴇ ᴇɴ github', sourceUrl: global.git, thumbnail: await (await fetch(pp)).buffer() }}})
+//m.react('🐶');
+  }
+};
+handler.help = ['menu'];
+handler.tags = ['menu'];
+handler.command = /^(prueba|menu7|menuhy)$/i;
+export default handler;
