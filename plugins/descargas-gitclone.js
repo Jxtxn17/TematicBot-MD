2a8a4cfb9738ad9e2e93752f78579a359b2c4d0c
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2024-03-31 15:11:51 -0500

    Actualizar descargas-gitclone.js

--- plugins/descargas-gitclone.js
+++ plugins/descargas-gitclone.js
@@ -3,13 +3,13 @@ import fetch from 'node-fetch'
 const regex = /(?:https|git)(?::\/\/|@)github\.com[\/:]([^\/:]+)\/(.+)/i
 var handler = async (m, { args, usedPrefix, command }) => {
 
-if (!args[0]) throw `*⚠️ ESCRIBA EL ENLACE DE UN REPOSITORIO DE GITHUB*\n\n❕ EJEMPLO\n*${usedPrefix + command}* https://github.com/Jxtxn17/BaileyBot-MD`
-if (!regex.test(args[0])) throw `*⚠️ ENLECE NO VALIDO*`
+if (!args[0]) throw `*🚫 𝐄𝐑𝐑𝐎𝐑 🚫 іᥒgrᥱsᥱ ᥙᥒ ᥱᥒᥣᥲᥴᥱ ძᥱ gі𝗍һᥙᑲ*\n\n[ 💡 ]\n*${usedPrefix + command}* https://github.com/Jxtxn17/BaileyBot-MD`
+if (!regex.test(args[0])) throw `*🚫 𝐄𝐑𝐑𝐎𝐑 🚫 ᥱᥒᥣᥲᥴᥱ іᥒ᥎ᥲᥣіძ᥆*`
 let [_, user, repo] = args[0].match(regex) || []
 repo = repo.replace(/.git$/, '')
 let url = `https://api.github.com/repos/${user}/${repo}/zipball`
 let filename = (await fetch(url, { method: 'HEAD' })).headers.get('content-disposition').match(/attachment; filename=(.*)/)[1]
-m.reply(`*🚀 D E S C A R G A N D O*`)
+m.reply(`*🚀 ᴄᴀʀɢᴀɴᴅᴏ ᴇsᴘᴇʀᴇ.. ツ*`)
 conn.sendFile(m.chat, url, filename, null, fliveLoc, m)
 
 }
