5fd774222ada8a6add0d19c9504d955f8f1643b8
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2024-03-31 14:49:41 -0500

    Actualizar cmd-del.js

--- plugins/cmd-del.js
+++ plugins/cmd-del.js
@@ -2,12 +2,12 @@ var handler = async (m, {conn, usedPrefix, text, command}) => {
   
 let hash = text
 if (m.quoted && m.quoted.fileSha256) hash = m.quoted.fileSha256.toString('hex')
-if (!hash) return conn.reply(m.chat, `[❗𝐈𝐍𝐅𝐎❗] *𝙄𝙉𝙂𝙍𝙀𝙎𝘼 𝙀𝙇 𝘾𝙊́𝘿𝙄𝙂𝙊*\n\nUtilice !listcmd`, m, fake, )
+if (!hash) return conn.reply(m.chat, `🚫 𝐄𝐑𝐑𝐎𝐑 🚫 *іᥒgrᥱsᥲ ᥱᥣ ᥴ᥆ძіg᥆*\n\nUtilice !listcmd`, m, fake, )
 
 let sticker = global.db.data.sticker
-if (sticker[hash] && sticker[hash].locked) return conn.reply(m.chat, '[❗𝐈𝐍𝐅𝐎❗]  *¡𝙁𝙐𝙉𝘾𝙄𝙊𝙉 𝙎𝙊𝙇𝙊 𝙋𝘼𝙍𝘼 𝙈𝙄 𝙋𝙍𝙊𝙋𝙄𝙀𝙏𝘼𝙍𝙄𝙊*', m, fake, )
+if (sticker[hash] && sticker[hash].locked) return conn.reply(m.chat, `🚫 𝐄𝐑𝐑𝐎𝐑 🚫  *𝖿ᥙᥒᥴі᥆ᥒ s᥆ᥣ᥆ ⍴ᥲrᥲ mі ძᥱsᥲrr᥆ᥣᥣᥲძ᥆r*', m, fake, )
 delete sticker[hash]
-conn.reply(m.chat, `✅ *𝙀𝙇𝙄𝙈𝙄𝙉𝘼𝘿𝙊 𝘾𝙊𝙍𝙍𝙀𝘾𝙏𝘼𝙈𝙀𝙉𝙏𝙀*`, m, fake, )
+conn.reply(m.chat, `✅ *ᥱᥣіmіᥒᥲძ᥆ ᥴ᥆rrᥱᥴ𝗍ᥲmᥱᥒ𝗍ᥱ.*`, m, fake, )
 
 }
 handler.tags = ['database']
