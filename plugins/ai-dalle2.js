85bb2691f41b27545422bf0f039d40425dbc325c
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2024-03-31 14:41:07 -0500

    Actualizar ai-dalle2.js

--- plugins/ai-dalle2.js
+++ plugins/ai-dalle2.js
@@ -2,31 +2,31 @@ import fetch from 'node-fetch'
 
 var handler = async (m, {conn, text, usedPrefix, command}) => {
 
-if (!text) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙄𝙉𝙂𝙍𝙀𝙎𝙀 𝙐𝙉 𝙏𝙀𝙓𝙏𝙊 𝙋𝘼𝙍𝘿 𝘾𝙍𝙀𝘼𝙍 𝙐𝙉𝘼 𝙄𝙈𝘼𝙂𝙀𝙉 𝙔 𝘼𝙎𝙄 𝙐𝙎𝙎𝙍 𝙇𝙎 𝙁𝙐𝙉𝘾𝙄𝙊𝙉 𝙀 𝘿𝘼𝙇𝙇𝙀-𝙀 2*\n\n❕ 𝙀𝙅𝙀𝙈𝙋𝙇𝙊\n*${usedPrefix + command}* gatitos llorando`
-await conn.sendMessage(m.chat, {text: '*❗ REALIZANDO IMAGEN, AGUARDE UN MOMENTO.*'}, {quoted: m})
+if (!text) throw `*🚫 𝐄𝐑𝐑𝐎𝐑 🚫 іᥒgrᥱsᥱ ᥙᥒ 𝗍ᥱ᥊𝗍᥆ ⍴ᥲrᥲ ᥴrᥱᥲr ᥙᥒᥲ іmᥲgᥱᥒ ᥡ ᥲsі́ ᥙsᥲr Dall-E 2*\n\n❕ ᥱȷᥱm⍴ᥣ᥆\n*${usedPrefix + command}* gatitos llorando`
+await conn.sendMessage(m.chat, {text: '*🚀 ᴄᴀʀɢᴀɴᴅᴏ ɪᴍᴀɢᴇɴ ッ, AGUARDE UN MOMENTO.*'}, {quoted: m})
 try {
 const tiores1 = await fetch(`https://vihangayt.me/tools/imagine?q=${text}`)
 const json1 = await tiores1.json();
 await conn.sendMessage(m.chat, {image: {url: json1.data}}, {quoted: m})
 } catch {
-console.log('[❗𝐈𝐍𝐅𝐎❗] 𝙀𝙍𝙍𝙊𝙍 𝙀𝙉 𝙇𝘼 𝘼𝙋𝙄 𝙉𝙐𝙈𝙀𝙍𝙊 1 𝘿𝙀 𝘿𝘼𝙇𝙇-𝙀 2.')
+console.log('🚫 𝐄𝐑𝐑𝐎𝐑 🚫 ᥱrr᥆r ᥱᥒ ᥣᥲ ᥲ⍴і ᥱᥒ ᥣᥲ ᥲ⍴і 1 ძᥱ Dall-E 2.')
 try {
 let tiores2 = await conn.getFile(`https://vihangayt.me/tools/midjourney?q=${text}`)
 await conn.sendMessage(m.chat, {image: {url: tiores2.data}}, {quoted: m})
 } catch {
-console.log('[❗𝐈𝐍𝐅𝐎❗] 𝙀𝙍𝙍𝙊𝙍 𝙀𝙉 𝙇𝘼 𝘼𝙋𝙄 𝙉𝙐́𝙈𝙀𝙍𝙊 2 𝘿𝙀 𝘿𝘼𝙇𝙇-E 2.');
+console.log('🚫 𝐄𝐑𝐑𝐎𝐑 🚫 ᥱrr᥆r ᥱᥒ ᥣᥲ ᥲ⍴і 2 Dall-E 2.');
 try {
 let tiores3 = await fetch(`https://vihangayt.me/tools/lexicaart?q=${text}`)
 let json3 = await tiores3.json()
 await conn.sendMessage(m.chat, {image: {url: json3.data[0].images[0].url}}, {quoted: m})
 } catch {
-console.log('[❗𝐈𝐍𝐅𝐎❗] 𝙀𝙍𝙍𝙊𝙍 𝙀𝙉 𝙇𝘼 𝘼𝙋𝙄 𝙉𝙐𝙈𝙀𝙍𝙊 3 𝘿𝘼𝙇𝙇-𝙀 2.')
+console.log('🚫 𝐄𝐑𝐑𝐎𝐑 🚫ᥱrr᥆r ᥱᥒ ᥣᥲ ᥲ⍴і 3 Dall-E 2.')
 try {
 const tiores4 = await conn.getFile(`https://api.lolhuman.xyz/api/dall-e?apikey=${lolkeysapi}&text=${text}`)
 await conn.sendMessage(m.chat, {image: {url: tiores4.data}}, {quoted: m})
 } catch {
-console.log('[❗𝐈𝐍𝐅𝐎❗] 𝙀𝙍𝙍𝙊𝙏, 𝙉𝙄𝙉𝙂𝙐𝙉𝘼 𝘼𝙋𝙄 𝙁𝙐𝙉𝘾𝙄𝙊𝙉𝘼.')
-throw `* 𝙀𝙍𝙍𝙊𝙍, 𝙉𝙊 𝙎𝙀 𝙊𝘽𝙏𝙐𝙑𝙄𝙀𝙍𝙊𝙉 𝙍𝙀𝙎𝙐𝙇𝙏𝘼𝘿𝙊𝙎.*`
+console.log('🚫 𝐄𝐑𝐑𝐎𝐑 🚫 ᥱrr᥆r, ᥒіᥒgᥙᥒᥲ ᥲ⍴і 𝖿ᥙᥒᥴі᥆ᥒᥲ..')
+throw `* ᥱrr᥆r, ᥒ᥆ sᥱ ᥆ᑲ𝗍ᥙ᥎іᥱr᥆ᥒ rᥱsᥙᥣ𝗍ᥲძ᥆s.*`
 }}}}
 
 }
