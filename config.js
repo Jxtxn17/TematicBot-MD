bce927d7bd87a6d048122ff5704e22ff23e576e0
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2024-04-07 18:44:46 -0500

    Actualizar config.js

--- config.js
+++ config.js
@@ -17,13 +17,11 @@ import { watchFile, unwatchFile } from 'fs'
 ['573106040746', '𝙴𝙳𝙴𝚁', true],
 ['51992004117', 'HJ', true],
 ['51955918117'],
-['595992809980'],
 ['50499698072'],
 ['5492215034412'],
 ['5217294888993'],
 ['5214437863111'],
 ['51971959440'],
-['593939005387']],
 //BETA: Si quiere evitar escribir el número que será bot en la consola, agregué desde aquí entonces: 
  //Sólo aplica para opción 2 (ser bot con código de texto de 8 digitos) 
  global.botNumberCode = '' //Ejemplo: +59309090909 
