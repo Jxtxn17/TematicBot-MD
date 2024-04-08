4386c4def06b2dd28f350bc4cdee139d2a004e41
Ender Tallyclerk <142455051+Ender-GB-Isis777@users.noreply.github.com>
2024-04-07 21:52:37 -0600

    Actualizar config.js

--- config.js
+++ config.js
@@ -21,14 +21,14 @@ import { watchFile, unwatchFile } from 'fs'
 ['5492215034412'],
 ['5217294888993'],
 ['5214437863111'],
-['51971959440'],
+['51971959440']],
 //BETA: Si quiere evitar escribir el número que será bot en la consola, agregué desde aquí entonces: 
  //Sólo aplica para opción 2 (ser bot con código de texto de 8 digitos) 
  global.botNumberCode = '' //Ejemplo: +59309090909 
  global.confirmCode = ''
 
  global.animxscans = ['51929972576'] 
- global.suittag = ['51929972576']];
+ global.suittag = ['51929972576']
  global.mods = [] 
  global.prems = []
 
