fdf28f73aa554e56aacae7b572ff036074a73442
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2024-04-11 16:18:38 -0500

    Actualizar owner-update.js

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -5,7 +5,7 @@ if (global.conn.user.jid == conn.user.jid) {
 let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''))
 conn.reply(m.chat, stdout.toString(), m)}
 //} catch {
-var update = execSync('git remote set-url origin https://github.com/DanielDiod/DokiBot.git && git pull')
+var update = execSync('git remote set-url origin https://github.com/Jxtxn17/BaileyBot-MD.git && git pull')
 await m.reply(update.toString())
 } catch {
 await m.reply(`${fg}`) 
