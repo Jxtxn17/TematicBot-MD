ad52dffe7015851c7343f78241f89d8f6c20cdbc
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2024-04-17 20:06:45 -0500

    Actualizar prueba-gt.js

--- plugins/prueba-gt.js
+++ plugins/prueba-gt.js
@@ -1,8 +1,1 @@
-client.on('message', message => {
-    console.log(message.body);
-	if(message.body === 'Como estas?') {
-		client.sendMessage(message.from, 'Bien y tu?:.');
-	}
-});
-
-client.initialize();
+..
