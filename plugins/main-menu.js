576660f1c44be9d18e027c89235bc795670b48a7
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2024-04-18 22:23:02 -0500

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -189,4 +189,4 @@ function clockString(ms) {
   let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
   return [d, 'd ', h, 'h ', m, 'm '].map(v => v.toString().padStart(2, 0)).join('')
 }
-\*
+*\
