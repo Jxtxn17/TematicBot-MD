b5b4a8e3ae0945d198e51419d92462d14a41ce29
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2024-04-20 22:47:53 -0500

    Update pepon.js

--- plugins/pepon.js
+++ plugins/pepon.js
@@ -4,7 +4,7 @@ import fs from "fs"
  let bot = `Peponnnn` 
  let vn = [img8].getRandom() 
  let chat = global.db.data.chats[m.chat] 
- if (/^bot$/i.test(m.text) && !chat.isBanned) {  
+ if (/^pepon$/i.test(m.text) && !chat.isBanned) {  
  conn.sendPresenceUpdate('recording', m.chat)     
  conn.sendFile(m.chat, vn, 'bot.jpg', pepon, m, true, { type: 'conversation', ptt: true, sendEphemeral: true, quoted: estilo })} 
  return !0 
