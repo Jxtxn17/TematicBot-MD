bb3412e3cfe14a7c23149cd56b5b6f3c03354685
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2024-04-20 23:05:05 -0500

    Update store.js

--- store.js
+++ store.js
@@ -37,8 +37,8 @@ global.img5 = 'https://telegra.ph/file/a34ebad4c9b5963c82f1a.jpg'
 global.img6 = 'https://telegra.ph/file/4e69f6c48d99f6c1174a9.jpg'
 global.img7 = 'https://telegra.ph/file/951275803faa754aff010.jpg'
 global.img8 = 'https://telegra.ph/file/9f765ea386a9dd1a5f8e5.jpg'
-
-global.ImgAll = [img, img1, img2, img3, img4, img5, img6, img7, img8]
+global.img9 = 'https://telegra.ph/file/011be97f80f19286ee4c7.jpg'
+global.ImgAll = [img, img1, img2, img3, img4, img5, img6, img7, img8, img9]
 
 //Enlaces
 global.yt = 'https://www.youtube.com/@Azami_Mods'
