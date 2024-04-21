751aef63a4f95f7b3399aeb1bb652c5ca5ed0e18
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2024-04-20 23:03:45 -0500

    Update pepon2.js

--- plugins/pepon2.js
+++ plugins/pepon2.js
@@ -2,8 +2,7 @@ let handler = async (m, { isPrems, conn }) => {
 let time = global.db.data.users[m.sender].lastcofre + 0 // 36000000 10 Horas //86400000 24 Horas
 if (new Date - global.db.data.users[m.sender].lastcofre < 0) throw `[❗𝐈𝐍𝐅𝐎❗] 𝚈𝙰 𝚁𝙴𝙲𝙻𝙰𝙼𝙰𝚂𝚃𝙴 𝚃𝚄 𝙲𝙾𝙵𝚁𝙴\𝚗𝚅𝚄𝙴𝙻𝚅𝙴 𝙴𝙽 *${msToTime(time - new Date())}* 𝙿𝙰𝚁𝙰 𝚅𝙾𝙻𝚅𝙴𝚁 𝙰 𝚁𝙴𝙲𝙻𝙰𝙼𝙰𝚁`
 
-let img = 'https://telegra.ph/file/9f765ea386a9dd1a5f8e5.jpg'
-let img2 = 'https://telegra.ph/file/011be97f80f19286ee4c7.jpg'
+let img = [img8].getRandom() 
 let dia = Math.floor(Math.random() * 30)
 let tok = Math.floor(Math.random() * 10)
 let hadesb = Math.floor(Math.random() * 4000)
