f6ad156d243cc325f412eceacb7c9e42a6ee2ad5
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2024-04-28 10:56:25 -0500

    Actualizar info-bot.js

--- plugins/info-bot.js
+++ plugins/info-bot.js
@@ -6,7 +6,5 @@ import fs from "fs"
  let chat = global.db.data.chats[m.chat] 
  if (/^bot$/i.test(m.text) && !chat.isBanned) {  
  conn.sendPresenceUpdate('composing', m.chat)     
- conn.sendFile(m.chat, vn, 'bot.jpg', bot, m, true, { type: 'conversation', ptt: true, sendEphemeral: true, quoted: estilo })} 
- return !0 
  } 
  export default handler
