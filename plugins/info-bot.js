8804e80c72a37add8b65eba324c44fcaccef6d7e
Jxtxn17 <140928688+Jxtxn17@users.noreply.github.com>
2024-04-28 21:36:49 -0500

    Delete plugins/info-bot.js

--- plugins/info-bot.js
+++
@@ -1,8 +0,0 @@
- let handler = m => m 
- handler.all = async function (m) { 
- let bot = `*🚀 Hola soy BaileyBot*\nEn que te puedo ayudar?` 
- let vn = [imagen1, imagen2, imagen3, imagen4, img1].getRandom() 
- let vn = [imagen1, imagen2, imagen3 img1].getRandom() 
- let chat = global.db.data.chats[m.chat] 
- if (/^bot$/i.test(m.text) && !chat.isBanned) {  
- conn.sendPresenceUpdate('recording', m.chat)     
